58eb9dfd966e76238311d5e809cd827a6ec640ae
O'Dane Brissett <brissettor@jncb.com>
2023-03-23 17:04:08 -0500

    latest addition

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ id: 1 });
 
-user.set({ name: 'newname', age: 999 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.fetch()
